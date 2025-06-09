900deb9b58a92e2fc0835a49cd70cab330d2a499
Erivelton Matos <eriveltonsdmatos@gmail.com>
2025-06-09 14:46:14 -0300

    updating README

--- src/main/java/com/example/eventsapi/EventsApiApplication.java
+++ src/main/java/com/example/eventsapi/EventsApiApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "API de Eventos", version = "1.0", description = "API para gerenciar eventos"))
 public class EventsApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EventsApiApplication.class, args);
